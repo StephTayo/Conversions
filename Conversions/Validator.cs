68c3a5b5e696846580ff73b1d432cc1bd3ff3fac
Stephen Omotayo <127545231+StephTayo@users.noreply.github.com>
2023-05-11 13:50:06 -0600

    Add files via upload

---
+++ Conversions/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Conversions
+{
+    /// <summary>
+    /// a repository of validation methods
+    /// </summary>
+    /// <param name="textBox">text box to check</param>
+    public static class Validator
+    {
+        public static bool IsValid(TextBox textbox) 
+        {
+            bool isValid = true;
+            if(textbox.Text=="")//bad
+            {
+                MessageBox.Show( textBox.Tag + "is required")
+                textBox.focus();
+                 isValid=false;
+            }
+           return isValid 
+        }
+
+    }
+}
